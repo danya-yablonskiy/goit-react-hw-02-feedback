c1af134b36dfbf9286a54aa3c91c24c36a8b1137
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-05-04 17:44:51 +0300

    Create Statistics.jsx

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,9 @@
+export const Statistics = ({good, neutral,bad, total, positivePercentage }) =>{
+    return <ul>
+                <li>Good: {good}</li>
+                <li>Neutral: {neutral}</li>
+                <li>Bad: {bad}</li>
+                <li>Total: {total}</li>
+                <li>Positive feedback: {positivePercentage}%</li>
+            </ul>
+};
